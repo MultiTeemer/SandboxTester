d2d2218981212b5e1c7ee6983ca2d2c457b88608
monoklid <artem94@bk.ru>
2014-10-28 16:48:50 +0400

    add tests for cmd args syntax

---
+++ cmdArgsTests/run.rb
@@ -0,0 +1,58 @@
+require 'test/unit'
+require './utils.rb'
+
+class CmdArgsTests < Utils::SpawnerTester
+
+  private
+
+  def compare_with_none_error(expected, rpt, test_order)
+    puts rpt if expected != (rpt[Utils::SPAWNER_ERROR_FIELD] == Utils::NONE_ERROR_SP_ERROR)
+    aseq(expected, rpt[Utils::SPAWNER_ERROR_FIELD] == Utils::NONE_ERROR_SP_ERROR, test_order)
+  end
+
+  def error_on_execute?(rpt, test_order = -1)
+    compare_with_none_error(false, rpt, test_order)
+  end
+
+  def execute_success?(rpt, test_order = -1)
+    compare_with_none_error(true, rpt, test_order)
+  end
+
+  public
+
+  def test_args_combinations
+    args = Utils.spawner.cmd_args
+    [1, nil].each do |test_order|
+      (1..args.size).each do |length|
+        args.combination(length).each do |combination|
+          args = {}
+          combination.each { |k| args[k] = 'something_wrong' }
+          error_on_execute?(run_spawner_test(test_order, args), length)
+        end
+      end
+    end
+
+    error_on_execute?(run_spawner_test)
+  end
+
+  def test_args_multipliers
+    stuff = [
+        {
+            :order => 1,
+            :func => Proc.new { |rpt| execute_success?(rpt) },
+        },
+        {
+            :order => nil,
+            :func => Proc.new { |rpt| error_on_execute?(rpt) },
+        },
+    ]
+    stuff.each do |item|
+      Utils.spawner.cmd_args_multipliers.each do |cat, arr|
+        arr.push('').each do |mult|
+          item[:func].call(run_spawner_test(item[:order], {cat => "1#{mult} "}))
+        end
+      end
+    end
+  end
+
+end
