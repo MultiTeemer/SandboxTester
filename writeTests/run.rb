bfcffc2c201612a134de71eaeac1b2dfc9afdda7
monoklid <artem94@bk.ru>
2014-11-01 15:58:47 +0400

    fix write tests

--- writeTests/run.rb
+++ writeTests/run.rb
@@ -4,13 +4,14 @@ require './utils.rb'
 class WriteTests < Utils::SpawnerTester
 
   def test_write
-    asindel(1, self.run_spawner_test(1)[Utils::WRITTEN_FIELD], 0.1, 0)
+    tests_count.each do |test_order|
+      asindel(1, self.run_spawner_test(test_order)[Utils::WRITTEN_FIELD], 0.1, 0)
+    end
   end
 
   def test_write_limit
     tests_count.each do |test_order|
-      exit_success?(self.run_spawner_test(test_order))
-      aseq(Utils::WRITE_LIMIT_EXCEEDED_RESULT, self.run_spawner_test(1, { :wl => '1kB' })[Utils::TERMINATE_REASON_FIELD], test_order)
+      aseq(Utils::WRITE_LIMIT_EXCEEDED_RESULT, self.run_spawner_test(test_order, { :wl => '1kB' })[Utils::TERMINATE_REASON_FIELD], test_order)
     end
   end
 
