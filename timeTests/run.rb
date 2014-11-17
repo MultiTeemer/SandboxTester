6bc97c554e227725a23d9a692a0ac7c94cc98b54
monoklid <artem94@bk.ru>
2014-11-17 22:30:45 +0400

    fix TimeTests::test_time_limit: use time_limit arg

--- timeTests/run.rb
+++ timeTests/run.rb
@@ -15,11 +15,7 @@ class TimeTests < Utils::SpawnerTester
   end
 
   def test_time_limit
-    params = [
-        { :time_limit => 0.5 },
-        { :deadline => 0.5 },
-        { :deadline => 0.3 },
-    ]
+    params = [ { :time_limit => 1 } ] * 3
     params.each_index do |i|
       rpt = self.run_spawner_test(i + 1, params[i])
       aseq(Utils::TIME_LIMIT_EXCEEDED_RESULT, rpt[Utils::TERMINATE_REASON_FIELD], i)
