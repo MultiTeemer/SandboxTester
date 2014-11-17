62abd5ba4b66fa7125e7bb2ea498948864f2d73a
monoklid <artem94@bk.ru>
2014-11-17 15:38:53 +0400

    increase delta for user time in BaseTests::test_main
    Sometimes this test fails.

--- baseTests/run.rb
+++ baseTests/run.rb
@@ -7,7 +7,7 @@ class BaseTests < Utils::SpawnerTester
     (1..4).each do |i|
       rprt = run_spawner_test(i)
       exit_success?(rprt)
-      asindel(0, rprt[Utils::USER_TIME_FIELD], 1e-3, i)
+      asindel(0, rprt[Utils::USER_TIME_FIELD], 1e-2, i)
       asindel(0, rprt[Utils::WRITTEN_FIELD], 1e-3, i)
       asindel(0, rprt[Utils::PEAK_MEMORY_USED_FIELD], 3e-1, i)
     end
