5caef8bae3cbf0cdff762cfc143eae3a00bd7854
monoklid <artem94@bk.ru>
2014-11-17 18:34:45 +0400

    use getters for args values in tests

--- cmdArgsTests/run.rb
+++ cmdArgsTests/run.rb
@@ -5,6 +5,10 @@ class CmdArgsTests < Utils::SpawnerTester
 
   private
 
+  def create_temporary_file
+    File.open(Utils.spawner.tmp_file_name, 'w') { |f| f.write('some data') }
+  end
+
   def compare_with_none_error(expected, rpt, test_order)
     aseq(expected, rpt[Utils::SPAWNER_ERROR_FIELD] == Utils::NONE_ERROR_SP_ERROR, test_order)
   end
@@ -34,13 +38,16 @@ class CmdArgsTests < Utils::SpawnerTester
 
   def test_args_combinations
     args = Utils.spawner.cmd_args
+    create_temporary_file
     stuff.each do |item|
       (0..args.size).each do |length|
         args.combination(length).each do |combination|
           correct_args, wrong_args = {}, {}
-          combination.each { |k| correct_args[k], wrong_args[k] = 1, 'something_wrong' }
+          combination.each do |k|
+            correct_args[k] = Utils.spawner.get_correct_value_for(k)
+            wrong_args[k] = Utils.spawner.get_wrong_value_for(k)
+          end
           item[:func].call(run_spawner_test(item[:order], correct_args), length)
-          error_on_execute?(run_spawner_test(item[:order], wrong_args))
           error_on_execute?(run_spawner_test(item[:order], wrong_args)) if wrong_args.size > 0
         end
       end
@@ -83,11 +90,15 @@ class CmdArgsTests < Utils::SpawnerTester
 
   def test_flags_args_combinations
     args, flags = Utils.spawner.cmd_args, Utils.spawner.cmd_flags
+    create_temporary_file
     stuff.each do |item|
       (0..args.size).each do |args_count|
         args.combination(args_count).each do |args_arr|
           run_args, error_args = {}, {}
-          args_arr.each { |arg| run_args[arg.to_sym], error_args[arg.to_sym] = 1, 'something_wrong' }
+          args_arr.each do |arg|
+            run_args[arg.to_sym]  = Utils.spawner.get_correct_value_for(arg)
+            error_args[arg.to_sym] = Utils.spawner.get_wrong_value_for(arg)
+          end
           (0..flags.size).each do |flags_count|
             flags.combination(flags_count).each do |run_flags|
               item[:func].call(run_spawner_test(item[:order], run_args, run_flags))
